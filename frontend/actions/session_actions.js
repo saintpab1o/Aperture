9719ec0b9f0ee01be2ba970e11f6ef2435e3e9df
saintpablo <pau1magic@aim.com>
2020-06-03 15:58:36 -0400

    Fixed Phase 1

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,25 @@
+import * as SessionUtil from '../util/session';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const createNewUser = formUser => dispatch => SessionUtil.signup(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => SessionUtil.login(formUser)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => SessionUtil.logout()
+  .then(() => dispatch(logoutCurrentUser()));
--- frontend/aperture.jsx
+++ frontend/aperture.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+
+import React from 'react'
 import ReactDOM from "react-dom";
-import configureStore from './store/store';
+import configureStore from "./store/store";
+import Root from "./components/root";
+import * as SessionUtil from './util/session.js'
 
 document.addEventListener("DOMContentLoaded", () => {
-  // const store = configureStore();
-  const rootEl = document.getElementById("root");
-  ReactDOM.render(<h1>Aperture </h1>, rootEl);
+  const store = configureStore();
+
+ // // TESTING START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.ajax = SessionUtil;
+  // //  TESTING END
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
 });
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // import login from './actions/session';
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // window.login = login;
+//   const root = document.getElementById("root");
+//   ReactDOM.render(<h1>Aperture</h1>, root);
+// });
+
+
+  // // TESTING START
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // //  TESTING END
