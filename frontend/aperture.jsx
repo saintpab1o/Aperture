93d43882432d575eec7f34f28cf87a6104b3ebca
saintpablo <pau1magic@aim.com>
2020-06-05 10:05:50 -0400

    Updated Heroku bugs

--- frontend/aperture.jsx
+++ frontend/aperture.jsx
@@ -1,9 +1,8 @@
-
 import React from 'react'
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-// import * as SessionUtil from './util/session.js'
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -19,26 +18,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore(preloadedState);
   ReactDOM.render(<Root store={store} />, root);
 });
-
-//  // TESTING START
-//   window.getState = store.getState;
-//   window.dispatch = store.dispatch;
-//   window.ajax = SessionUtil;
-//  TESTING END
-
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// // import login from './actions/session';
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   // window.login = login;
-//   const root = document.getElementById("root");
-//   ReactDOM.render(<h1>Aperture</h1>, root);
-// });
-
-
-  // // TESTING START
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  // //  TESTING END
