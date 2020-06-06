719195fea63351cb45ed3220d8beae6fbac1f318
saintpablo <pau1magic@aim.com>
2020-06-05 22:02:51 -0400

    Added Mid Splash Text

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -32,27 +32,77 @@ const Splash = ({ currentUser, logout }) => {
 
 
      
-        <div  className= "splash-img">
-      
-           
-            <img src={window.splashURL} alt="splash-img" width="1400" height="600"></img>
+        <div  className="splash-img">
+          <img src={window.splashURL} alt="splash-img" width="1400" height="600"></img>
+        </div>
 
             <div className="splash-text-top">Discover and share</div>
             <div className="splash-text-mid">the world’s best</div>
             <div className="splash-text-bottom">photos</div>
             <div className="splash-mini-bottom">Get inspired with incredible photos from diverse styles and genres</div>
             <div className="splash-mini-bottom2">around the world. We're not guided by fads—just great photography.</div>
-        </div>
+       
 
-        <div>
+        
+
         <div className= "signup-button">
           <Link to="/signup">Sign up</Link>
           </div>
-        </div>
+
+
+      <div className="mid-text-header">What makes us different</div>
+    
+    <div className="mid-text-container">
+
+    
+      <div className="mid-text-grow">Grow as a photographer</div>
+      <div className="mid-text-build"> Build your career</div>
+      <div className="mid-text-performance">See how you're performing</div>
+      </div>
+
+     
+
+      <div className="mid-text-sub-container">
+
+        <div className="mid-text-grow-sub">
+          Get immediate exposure with your first upload.
+          <br></br> 
+          Our Pulse algorithm surfaces new 
+          <br></br>
+          photographs and photographers, ensuring
+          <br></br>
+           your photos are seen by the community so you
+           <br></br>
+            receive valuable feedback on day one.</div>
+
+      <div className="mid-text-build-sub">
+        Present yourself as a professional. Get hired 
+      <br></br>
+        by displaying your services, create a Directory
+      <br></br> 
+      listing, showcase the workshops you're 
+      <br></br>
+      holding, and create Galleries to highlight your 
+      <br></br>
+      work.</div>
+
+      
+      <div className="mid-text-performance-sub">
+        With Statistics and Pulse you get valuable 
+      <br></br>
+      insights into how your photos are performing
+      <br></br>
+       and how you rank in comparison to other 
+      <br></br>photographers in the community.</div>
+
+      </div>
+
+        
+
   
 
 
-
+      
 
 
     </div>
