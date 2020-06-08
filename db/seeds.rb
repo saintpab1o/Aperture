1ad35a5f6ba4e70d18768270b710ee1fafc3686d
saintpablo <pau1magic@aim.com>
2020-06-08 14:59:02 -0400

    updated seed in auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destoy_all
+
+
+demo_user = User.create(username: "pm", password: "password" email:"test@test.com")
