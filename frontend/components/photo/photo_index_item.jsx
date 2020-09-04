65ffebb1da6e33d9b8b74eaab4971f9751869c82
saintpablo <pau1magic@aim.com>
2020-09-04 14:00:00 +0100

    Changed Back To Functional

--- frontend/components/photo/photo_index_item.jsx
+++ frontend/components/photo/photo_index_item.jsx
@@ -2,21 +2,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-class PhotoIndexItem extends React.Component{
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return (
-             <div className='photo-feed'>
-                <Link to={`/photo/${this.props.photo.id}`}>
-                 <img className='photo-feed-item' src={this.props.photo.photoURL} />
-             </Link>
-            </div>
-         )
-    }
+const PhotoIndexItem = ({ photo }) => {
+    return (
+        <div className='photo-feed'>
+            <Link to={`/photo/${photo.id}`}>
+                <img className='photo-feed-item' src={photo.photoURL} />
+            </Link>
+        </div>
+    )
 }
 
 export default PhotoIndexItem;
