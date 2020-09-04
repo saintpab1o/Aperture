b0bc846dae0b906a8dae8abf57d6f498aec26aa0
saintpablo <pau1magic@aim.com>
2020-09-04 14:00:00 +0100

    ConvertedIndexItemToClass

--- frontend/components/photo/photo_index_item.jsx
+++ frontend/components/photo/photo_index_item.jsx
@@ -2,14 +2,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PhotoIndexItem = ({ photo }) => {
-    return (
-        <div className='photo-feed'>
-            <Link to={`/photo/${photo.id}`}>
-                <img className='photo-feed-item' src={photo.photoURL} />
-            </Link>
-        </div>
-    )
+
+class PhotoIndexItem extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+             <div className='photo-feed'>
+                <Link to={`/photo/${this.props.photo.id}`}>
+                 <img className='photo-feed-item' src={this.props.photo.photoURL} />
+             </Link>
+            </div>
+         )
+    }
 }
 
 export default PhotoIndexItem;
--- frontend/components/photo/photo_show_container.js
+++ frontend/components/photo/photo_show_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { fetchPhoto } from '../../actions/photo_actions';
 import PhotoShow from './photo_show';
-import { withRouter } from 'react-router';
+
 
 const mapStateToProps = (state, ownProps) => {
     return {
