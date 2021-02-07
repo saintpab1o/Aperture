a63aa692b4b8cc54327b35c4d25e58cc1019239c
saintpablo <pau1magic@aim.com>
2021-02-07 14:00:00 +0100

    ‘NewCommit’

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -72,7 +72,6 @@ class LoginForm extends React.Component {
              </div>
              </div>
             <div className="outerbox">
- 
             <div
                 className="form-container1">
                     <form>
