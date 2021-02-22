496256aaf0d29a3c9601689dd7b172bf9292af9e
saintpablo <pau1magic@aim.com>
2021-02-22 17:00:00 +0100

    Done For Night

--- frontend/components/photo/photo_show.jsx
+++ frontend/components/photo/photo_show.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 class PhotoShow extends React.Component {
     constructor(props) {
         super(props);
+        
     }
     componentDidMount() {
         this.props.fetchPhoto(this.props.match.params.photoId); 
--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -6,11 +6,12 @@ const photosReducer  = (state = {}, action) => {
     Object.freeze(state);
 
     switch (action.type) {
+        
         case RECEIVE_ALL_PHOTOS:
             return merge({}, state, action.photos);
 
         case RECEIVE_PHOTO: 
-            return merge({}, state, { [action.photo.id]: action.photo });
+            return merge({}, state, { [action.photo.id]: action.photo.photoUrl });
 
         default:
             return state;
