094353366c2b57041096064d810c6b2a7e02b723
saintpablo <pau1magic@aim.com>
2021-02-24 01:54:37 -0500

    Added Show Page Nav

--- frontend/components/photo/photo_show.jsx
+++ frontend/components/photo/photo_show.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAngellist } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 class PhotoShow extends React.Component {
@@ -14,8 +18,44 @@ class PhotoShow extends React.Component {
 
     render() { 
         const photo = this.props.photo;
-        return (
+
+        const welcomeMessage = () => (
             <div>
+                <div className="nav-container">
+                    <div className="splash-nav-header">
+                        <Link to="/">Aperture</Link>
+                    </div>
+                </div>
+
+                <div className="logout-button-container">
+                    <div className="upload-button-top">
+                        <Link to="/upload">Upload</Link>
+                    </div>
+
+                    <div>
+                        <button className="logout-button-top" onClick={this.props.logout}>
+                            Log Out
+            </button>
+                    </div>
+                </div>
+
+                <div className="home-feed-text">
+                    <h3>Home Feed</h3>
+                </div>
+
+                <div className="home-feed-text2">
+                    <h4>Featured photographers</h4>
+                </div>
+                <div className="home-feed-text3">
+                    <p>Find photographers to follow</p>
+                </div>
+            </div>
+        )
+
+        
+        return (    
+            <div>
+                {welcomeMessage()}
                 <div className="photo-feed">
                     <div className="photo">
                         <img className='photo-feed-item' src={photo} />
